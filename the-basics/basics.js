847ead7c20825cd71f8808e0c836a1cbc7f691df
Assami Muzaki <muzakiassami@gmail.com>
2023-02-09 21:47:55 +0700

    the basics

---
+++ the-basics/basics.js
@@ -0,0 +1,13 @@
+var message = "hello world";
+message.toLowerCase();
+console.log(message);
+// ---- TYPE SCRIPT HAVE NO UNDEFINED ----
+var user = {
+  name: "Daniel",
+  age: 26,
+};
+// user.location;
+//  ---- TEST RANDOM ----
+console.log(Math.random());
+
+console.log(typeof new Date());
---
+++ the-basics/basics.ts
@@ -0,0 +1,16 @@
+let message = "hello world";
+
+message.toLowerCase();
+console.log(message);
+
+// ---- TYPE SCRIPT HAVE NO UNDEFINED ----
+const user = {
+  name: "Daniel",
+  age: 26,
+};
+
+// user.location;
+
+//  ---- TEST RANDOM ----
+
+console.log(Math.random());
